48ba3f7f258e12332346661abdab0e6424cfee58
armandr <pi@raz-pi2.(none)>
2015-03-26 20:22:23 +0000

    plays mp3s and wavs.

---
+++ audio.js
@@ -0,0 +1,17 @@
+// audio.js
+
+function audio()
+{
+  if (!(this instanceof audio))
+    return new audio();
+
+  console.log('loaded audio module');
+};
+
+var exec = require('child_process').exec;
+module.exports = audio;
+
+audio.prototype.play = function (fileName)
+{
+  exec("mpg123 "+ fileName, null);
+}
